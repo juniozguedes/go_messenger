b47741d7f451bd8555476110e1fb5b1e269b6d66
juniozguedes <juniozguedes@gmail.com>
2020-07-27 20:11:53 -0300

    Procfile

--- hub.go
+++ hub.go
@@ -1,7 +1,3 @@
-// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 package main
 
 // Hub maintains the set of active clients and broadcasts messages to the
